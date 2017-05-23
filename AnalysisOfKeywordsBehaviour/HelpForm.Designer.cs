4162bba013ede95ca5b8edc1e2c3835bc4dd9bc8
User <User@NATALI>
2017-05-24 01:40:32 +0300

    Add help form for editing lists with experimental data.

---
+++ AnalysisOfKeywordsBehaviour/HelpForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace AnalysisOfKeywordsBehaviour
+{
+    partial class HelpForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.tbx = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(169, 242);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(96, 37);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(40, 242);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(96, 37);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Сохранение";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // tbx
+            // 
+            this.tbx.Location = new System.Drawing.Point(12, 12);
+            this.tbx.Multiline = true;
+            this.tbx.Name = "tbx";
+            this.tbx.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbx.Size = new System.Drawing.Size(282, 208);
+            this.tbx.TabIndex = 3;
+            // 
+            // HelpForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 292);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.tbx);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "HelpForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Изменить...";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox tbx;
+    }
+}
---
+++ AnalysisOfKeywordsBehaviour/HelpForm.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnalysisOfKeywordsBehaviour
+{
+    public partial class HelpForm : Form
+    {
+        /// <summary>
+        /// Поле, соответствующее экземпляру класса главной формы.
+        /// </summary>
+        private MainForm _mainForm;
+        /// <summary>
+        /// Номер списка с экспериментальными данными.
+        /// </summary>
+        private int _numOfList;
+
+        /// <summary>
+        /// Конструктор класса.
+        /// </summary>
+        /// <param name="mainForm">Главная (родительская) форма.</param>
+        /// <param name="numOfList">Номер списка с экспериментальными данными, которые необходимо изменить.</param>
+        public HelpForm(MainForm mainForm, int numOfList)
+        {
+            InitializeComponent();
+            _mainForm = mainForm;
+            _numOfList = numOfList;
+            tbx.Text = "";
+            //выводим соответствующий список с экспериментальными данными
+            switch (_numOfList)
+            {
+                case 0:
+                    foreach (string word in _mainForm.AllWords)
+                        tbx.Text += word + Environment.NewLine;
+                    break;
+                case 1:
+                    foreach (string word in _mainForm.Markems)
+                        tbx.Text += word + Environment.NewLine;
+                    break;
+                case 2:
+                    foreach (string word in _mainForm.Definitions)
+                        tbx.Text += word + Environment.NewLine;
+                    break;
+                case 3:
+                    foreach (string word in _mainForm.FreeAssociations)
+                        tbx.Text += word + Environment.NewLine;
+                    break;
+                case 4:
+                    foreach (string word in _mainForm.DirectAssociations)
+                        tbx.Text += word + Environment.NewLine;
+                    break;
+                case 5:
+                    foreach (string word in _mainForm.Similarities)
+                        tbx.Text += word + Environment.NewLine;
+                    break;
+                case 6:
+                    foreach (string word in _mainForm.Opposities)
+                        tbx.Text += word + Environment.NewLine;
+                    break;
+                case 7:
+                    foreach (string word in _mainForm.ImportantWords)
+                        tbx.Text += word + Environment.NewLine;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Сохраняет измнения и возвращает управление на главную форму.
+        /// </summary>
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            switch (_numOfList)
+            {
+                case 0:
+                    _mainForm.AllWords.Clear();
+                    _mainForm.dataGridViewAllWords.Rows.Clear();
+                    for (int i = 0; i < tbx.Lines.Length; i++)
+                    {
+                        _mainForm.AllWords.Add(tbx.Lines[i]);
+                        _mainForm.dataGridViewAllWords.Rows.Add(tbx.Lines[i]);
+                    }
+                    break;
+                case 1:
+                    _mainForm.Markems.Clear();
+                    _mainForm.dataGridViewWords.Rows.Clear();
+                    for (int i = 0; i < tbx.Lines.Length; i++)
+                    {
+                        _mainForm.Markems.Add(tbx.Lines[i]);
+                        _mainForm.dataGridViewWords.Rows.Add(tbx.Lines[i]);
+                    }
+                    break;
+                case 2:
+                    _mainForm.Definitions.Clear();
+                    for (int i = 0; i < tbx.Lines.Length; i++)
+                        _mainForm.Definitions.Add(tbx.Lines[i]);
+                    break;
+                case 3:
+                    _mainForm.FreeAssociations.Clear();
+                    for (int i = 0; i < textBox.Lines.Length; i++)
+                        _mainForm.FreeAssociations.Add(textBox.Lines[i]);
+                    break;
+                case 4:
+                    _mainForm.DirectAssociations.Clear();
+                    for (int i = 0; i < tbx.Lines.Length; i++)
+                        _mainForm.DirectAssociations.Add(tbx.Lines[i]);
+                    break;
+                case 5:
+                    _mainForm.Similarities.Clear();
+                    for (int i = 0; i < tbx.Lines.Length; i++)
+                        _mainForm.Similarities.Add(tbx.Lines[i]);
+                    break;
+                case 6:
+                    _mainForm.Opposities.Clear();
+                    for (int i = 0; i < tbx.Lines.Length; i++)
+                        _mainForm.Opposities.Add(tbx.Lines[i]);
+                    break;
+                case 7:
+                    _mainForm.ImportantWords.Clear();
+                    for (int i = 0; i < tbx.Lines.Length; i++)
+                        _mainForm.ImportantWords.Add(tbx.Lines[i]);
+                    break;
+            }
+            Close();
+        }
+
+        /// <summary>
+        /// Отменяет измнения и возвращает управление на главную форму.
+        /// </summary>
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
